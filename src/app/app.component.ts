7514a426f156cfd2ea047c338a8d938cd23421ff
Ravindra Dhore <ravindrad@ansh.com>
2019-11-25 12:45:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import countries from './countries.json';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'firebase-auth-angular-app';
+  public countryList: {name: string, code: string}[] = countries;
 }
